d72a25b25a3824c042162504797f011b5ac7caba
n2gabriellerosa <gabrielledeoliveira982@gmail.com>
2024-11-22 10:34:09 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header/header'
+import './global.scss'
+
+export default function App(){
+  return (
+    <>
+    <Header></Header>
+    
+    </>
+  )
+
+
+}
---
+++ src/Pages/Inicio/Inicio.jsx
@@ -0,0 +1,12 @@
+import S from "./inicio.module.scss"
+
+
+export default function inicio(){
+    return(
+        <section>
+            <section className={S.post}>
+                <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+        </section>
+    )
+}
